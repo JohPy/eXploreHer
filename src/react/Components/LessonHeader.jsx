63142a9c8e22f272f64a4321c57febe090f8466e
lptm-ng <lptmnguyen@gmail.com>
2025-07-01 13:54:21 +0200

    Add State Logic to Lesson Stepper
    changes include:
    - instead of using MUI MobileStepper (only two states), custom "Stepper" with 4 states

--- src/react/Components/LessonHeader.jsx
+++ src/react/Components/LessonHeader.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Box, MobileStepper, IconButton, Typography } from '@mui/material'
+import { Box, IconButton, Typography } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
 
-const LessonHeader = ({ onExit, activeStep, maxSteps }) => (
+const statusStyles = {
+  correct: {
+    backgroundColor: '#3F8E76',
+    border: '1px solid #234F42'
+  },
+  incorrect: {
+    backgroundColor: '#F44336',
+    border: '1px solid #B71C1C'
+  },
+  current: {
+    backgroundColor: '#D7D7D7',
+    border: '1px solid #7C7C7C'
+  },
+  unanswered: {
+    backgroundColor: 'transparent',
+    border: '1px solid #7C7C7C'
+  }
+}
+
+const LessonHeader = ({ onExit, activeStep, maxSteps, steps }) => (
   <Box
     sx={{
       display: 'flex',
       alignItems: 'center',
-      width: '90%',
+      width: '95%',
       p: 1
     }}
   >
@@ -16,30 +35,26 @@ const LessonHeader = ({ onExit, activeStep, maxSteps }) => (
       <CloseIcon />
     </IconButton>
 
-    <MobileStepper
-      variant="dots"
-      steps={maxSteps}
-      position="static"
-      activeStep={activeStep}
+    <Box
       sx={{
         flexGrow: 1,
-        justifyContent: 'center',
-        backgroundColor: 'transparent',
-        '& .MuiMobileStepper-dot': {
-          backgroundColor: '#D7D7D7',
-          border: '1px solid #7C7C7C',
-          width: '10px',
-          height: '10px',
-          margin: '0 4px'
-        },
-        '& .MuiMobileStepper-dotActive': {
-          backgroundColor: '#3F8E76',
-          border: '1px solid #234F42'
-        }
+        display: 'flex',
+        justifyContent: 'center'
       }}
-      nextButton={<span />}
-      backButton={<span />}
-    />
+    >
+      {steps.map((step) => (
+        <Box
+          key={step.id}
+          sx={{
+            width: '10px',
+            height: '10px',
+            borderRadius: '50%',
+            margin: '0 4px',
+            ...statusStyles[step.status]
+          }}
+        />
+      ))}
+    </Box>
 
     <Typography
       variant="body2"
@@ -57,7 +72,11 @@ const LessonHeader = ({ onExit, activeStep, maxSteps }) => (
 LessonHeader.propTypes = {
   onExit: PropTypes.func.isRequired,
   activeStep: PropTypes.number.isRequired,
-  maxSteps: PropTypes.number.isRequired
+  maxSteps: PropTypes.number.isRequired,
+  steps: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    status: PropTypes.oneOf(['correct', 'incorrect', 'current', 'unanswered']).isRequired
+  })).isRequired
 }
 
 export default LessonHeader
