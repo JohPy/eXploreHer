48c04cd7871112eb0d0fcd5d61b7acd2d3fdf37b
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-14 17:57:24 +0200

    Add WordMatchQuiz jsx and css
    ... and use it in MatchScreen

---
+++ src/react/Components/WordMatchQuiz.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './WordMatchQuiz.css'
+
+const WordMatchQuiz = () => (
+  <div>
+    <button
+      className="quiz-btn"
+      type="button"
+    >
+      Question
+    </button>
+    <button
+      className="quiz-btn"
+      type="button"
+    >
+      Answer
+    </button>
+  </div>
+)
+
+export default WordMatchQuiz
--- src/react/Pages/MatchScreen.jsx
+++ src/react/Pages/MatchScreen.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import WordMatchQuiz from '../Components/WordMatchQuiz'
+
 const MatchScreen = () => (
   <div>
     <h1>Match Screen</h1>
+    <WordMatchQuiz />
   </div>
 )
 
