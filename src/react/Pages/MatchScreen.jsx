a2cc96d70a05a33a5cf5c95d967ae6b9db946108
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-15 14:36:54 +0200

    Add container style to MatchScreen for improved layout

--- src/react/Pages/MatchScreen.jsx
+++ src/react/Pages/MatchScreen.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 import WordMatchQuiz from '../Components/WordMatchQuiz'
 
+const containerStyle = {
+  padding: '32px',
+  maxWidth: '600px',
+  margin: '40px auto',
+  boxSizing: 'border-box'
+}
+
 const MatchScreen = () => (
-  <div>
+  <div style={containerStyle}>
     <h2>Finde die passenden Paare</h2>
     <WordMatchQuiz />
   </div>
