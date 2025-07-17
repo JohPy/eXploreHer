7099bc6ec90a9706b54b3cac617f9ae3f30ecac7
lenglara <131978798+lenglara@users.noreply.github.com>
2025-07-17 18:16:12 +0200

    Change DropField style based on state (active, hovering, correct, wrong)

--- src/react/Components/DragAndDrop/DropField.jsx
+++ src/react/Components/DragAndDrop/DropField.jsx
@@ -3,8 +3,30 @@ import PropTypes from 'prop-types'
 import { useDroppable } from '@dnd-kit/core'
 import { Paper, Typography } from '@mui/material'
 
-const DropField = ({ id, label, item, sx = {} }) => {
+const DropField = ({ id, assigned, position, correct }) => {
   const { setNodeRef, isOver } = useDroppable({ id })
+
+  // Update styling based on state
+  const activeStyle = assigned ? { backgroundColor: '#fff' } : {}
+  const hoverStyle = isOver ? { opacity: 0.9, boxShadow: '0 0 0 2px rgba(0,0,0,0.2)' } : {}
+  let answerStyle = {}
+
+  switch (correct) {
+    case true:
+      answerStyle = {
+        border: (theme) => `2px solid ${theme.palette.correct.main}`,
+        color: (theme) => theme.palette.correct.main
+      }
+      break
+    case false:
+      answerStyle = {
+        border: (theme) => `2px solid ${theme.palette.wrong.main}`,
+        color: (theme) => theme.palette.wrong.main
+      }
+      break
+    default:
+      answerStyle = {}
+  }
 
   return (
     <Paper
@@ -21,11 +43,16 @@ const DropField = ({ id, label, item, sx = {} }) => {
         justifyContent: 'center',
         color: '#000',
         backgroundColor: 'berryGrey.light',
-        ...sx
+        ...position,
+        ...activeStyle,
+        ...hoverStyle,
+        ...answerStyle
+
       }}
     >
-      <Typography variant="body1">
-        {item || label}
+      <Typography variant="body2" fontWeight="bold">
+        {/* Show label of assigned card if available otherwise " " */}
+        { assigned?.label || ' ' }
       </Typography>
     </Paper>
   )
@@ -33,9 +60,9 @@ const DropField = ({ id, label, item, sx = {} }) => {
 
 DropField.propTypes = {
   id: PropTypes.string,
-  label: PropTypes.string,
-  item: PropTypes.any,
-  sx: PropTypes.any
+  assigned: PropTypes.any,
+  position: PropTypes.any.isRequired,
+  correct: PropTypes.bool
 }
 
 export default DropField
