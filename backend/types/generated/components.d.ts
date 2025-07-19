e54f60fad4ba1b283a346ac527a11afe390e82fa
Witali Klein <75163928+wklein1@users.noreply.github.com>
2025-07-19 22:20:33 +0200

    add course content, exercises representation

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,141 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface BuildingblocksAnswer extends Struct.ComponentSchema {
+  collectionName: 'components_buildingblocks_answers';
+  info: {
+    displayName: 'Answer';
+    icon: 'file';
+  };
+  attributes: {
+    IsCorrect: Schema.Attribute.Boolean &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<false>;
+    Text: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface BuildingblocksImageDropfield extends Struct.ComponentSchema {
+  collectionName: 'components_buildingblocks_image_dropfields';
+  info: {
+    displayName: 'Image Dropfield';
+    icon: 'expand';
+  };
+  attributes: {
+    Term: Schema.Attribute.String & Schema.Attribute.Required;
+    XCoord: Schema.Attribute.Decimal &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<0>;
+    YCoord: Schema.Attribute.Decimal &
+      Schema.Attribute.Required &
+      Schema.Attribute.DefaultTo<0>;
+  };
+}
+
+export interface BuildingblocksPair extends Struct.ComponentSchema {
+  collectionName: 'components_buildingblocks_pairs';
+  info: {
+    displayName: 'Text Pair';
+    icon: 'server';
+  };
+  attributes: {
+    Answer: Schema.Attribute.Text;
+    Question: Schema.Attribute.Text;
+  };
+}
+
+export interface ExercisesDragAndDrop extends Struct.ComponentSchema {
+  collectionName: 'components_exercises_drag_and_drops';
+  info: {
+    displayName: 'Image Drag and Drop';
+    icon: 'cursor';
+  };
+  attributes: {
+    Dropfields: Schema.Attribute.Component<
+      'buildingblocks.image-dropfield',
+      true
+    > &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMax<
+        {
+          min: 1;
+        },
+        number
+      >;
+    Image: Schema.Attribute.Media<'images'>;
+  };
+}
+
+export interface ExercisesFillInTheBlank extends Struct.ComponentSchema {
+  collectionName: 'components_exercises_fill_in_the_blanks';
+  info: {
+    displayName: 'Fill-in-the-Blank';
+    icon: 'underline';
+  };
+  attributes: {
+    ClozeText: Schema.Attribute.Blocks & Schema.Attribute.Required;
+  };
+}
+
+export interface ExercisesMatchPairs extends Struct.ComponentSchema {
+  collectionName: 'components_exercises_match_pairs';
+  info: {
+    displayName: 'Match Pairs';
+    icon: 'dashboard';
+  };
+  attributes: {
+    Pairs: Schema.Attribute.Component<'buildingblocks.pair', true> &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMax<
+        {
+          min: 2;
+        },
+        number
+      >;
+  };
+}
+
+export interface ExercisesMultipleChoice extends Struct.ComponentSchema {
+  collectionName: 'components_exercises_multiple_choices';
+  info: {
+    displayName: 'Multiple Choice';
+    icon: 'check';
+  };
+  attributes: {
+    Answers: Schema.Attribute.Component<'buildingblocks.answer', true> &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMax<
+        {
+          min: 2;
+        },
+        number
+      >;
+    Question: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface ExercisesShortAnswer extends Struct.ComponentSchema {
+  collectionName: 'components_exercises_short_answers';
+  info: {
+    displayName: 'Short Answer';
+    icon: 'italic';
+  };
+  attributes: {
+    Answer: Schema.Attribute.String & Schema.Attribute.Required;
+    Question: Schema.Attribute.Text & Schema.Attribute.Required;
+  };
+}
+
+export interface ExercisesTextDragAndDrop extends Struct.ComponentSchema {
+  collectionName: 'components_exercises_text_drag_and_drops';
+  info: {
+    displayName: 'Text Drag and Drop';
+    icon: 'bold';
+  };
+  attributes: {
+    ClozeText: Schema.Attribute.Blocks & Schema.Attribute.Required;
+  };
+}
+
 export interface SharedMedia extends Struct.ComponentSchema {
   collectionName: 'components_shared_media';
   info: {
@@ -65,6 +201,15 @@ export interface SharedSlider extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'buildingblocks.answer': BuildingblocksAnswer;
+      'buildingblocks.image-dropfield': BuildingblocksImageDropfield;
+      'buildingblocks.pair': BuildingblocksPair;
+      'exercises.drag-and-drop': ExercisesDragAndDrop;
+      'exercises.fill-in-the-blank': ExercisesFillInTheBlank;
+      'exercises.match-pairs': ExercisesMatchPairs;
+      'exercises.multiple-choice': ExercisesMultipleChoice;
+      'exercises.short-answer': ExercisesShortAnswer;
+      'exercises.text-drag-and-drop': ExercisesTextDragAndDrop;
       'shared.media': SharedMedia;
       'shared.quote': SharedQuote;
       'shared.rich-text': SharedRichText;
--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -506,6 +506,63 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiChapterChapter extends Struct.CollectionTypeSchema {
+  collectionName: 'chapters';
+  info: {
+    displayName: 'Chapter';
+    pluralName: 'chapters';
+    singularName: 'chapter';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    lessons: Schema.Attribute.Relation<'oneToMany', 'api::lesson.lesson'>;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::chapter.chapter'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    Title: Schema.Attribute.String;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
+export interface ApiCourseContentCourseContent extends Struct.SingleTypeSchema {
+  collectionName: 'course_contents';
+  info: {
+    displayName: 'Course content';
+    pluralName: 'course-contents';
+    singularName: 'course-content';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    chapters: Schema.Attribute.Relation<'oneToMany', 'api::chapter.chapter'>;
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::course-content.course-content'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface ApiGlobalGlobal extends Struct.SingleTypeSchema {
   collectionName: 'globals';
   info: {
@@ -532,6 +589,51 @@ export interface ApiGlobalGlobal extends Struct.SingleTypeSchema {
     publishedAt: Schema.Attribute.DateTime;
     siteDescription: Schema.Attribute.Text & Schema.Attribute.Required;
     siteName: Schema.Attribute.String & Schema.Attribute.Required;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
+export interface ApiLessonLesson extends Struct.CollectionTypeSchema {
+  collectionName: 'lessons';
+  info: {
+    displayName: 'Lesson';
+    pluralName: 'lessons';
+    singularName: 'lesson';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    Exercises: Schema.Attribute.DynamicZone<
+      [
+        'exercises.multiple-choice',
+        'exercises.drag-and-drop',
+        'exercises.text-drag-and-drop',
+        'exercises.short-answer',
+        'exercises.match-pairs',
+        'exercises.fill-in-the-blank',
+      ]
+    > &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMax<
+        {
+          min: 1;
+        },
+        number
+      >;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::lesson.lesson'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    Title: Schema.Attribute.String;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
@@ -1051,7 +1153,10 @@ declare module '@strapi/strapi' {
       'api::article.article': ApiArticleArticle;
       'api::author.author': ApiAuthorAuthor;
       'api::category.category': ApiCategoryCategory;
+      'api::chapter.chapter': ApiChapterChapter;
+      'api::course-content.course-content': ApiCourseContentCourseContent;
       'api::global.global': ApiGlobalGlobal;
+      'api::lesson.lesson': ApiLessonLesson;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
