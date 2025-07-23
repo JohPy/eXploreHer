65ed84b94d4b72d33314aaf6f9f9d214698cc506
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-23 20:00:19 +0200

    Add support for word-match quiz in Exercise component

--- frontend/src/react/Components/Lesson/Exercise.jsx
+++ frontend/src/react/Components/Lesson/Exercise.jsx
@@ -2,11 +2,14 @@ import PropTypes from 'prop-types'
 import * as React from 'react'
 
 import DragAndDrop from '../DragAndDrop/DragAndDrop'
+import WordMatchQuiz from '../WordMatchQuiz'
 
 const Exercise = ({ exercise, onCorrectChange }) => {
   switch (exercise.type) {
     case 'drag-and-drop':
       return <DragAndDrop fields={exercise.fields} ImageComponent={exercise.ImageComponent} onCorrectChange={onCorrectChange} />
+    case 'word-match-quiz':
+      return <WordMatchQuiz questions={exercise.questions} onCorrectChange={onCorrectChange} />
     default:
       return <div> Test </div>
   }
