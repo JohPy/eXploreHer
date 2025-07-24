8871cc4d4c546e502fe12b44a499972ffe3c6dff
Witali Klein <75163928+wklein1@users.noreply.github.com>
2025-07-24 19:21:56 +0200

    add chapter-populate to chapter route middlewares

--- backend/src/api/chapter/routes/chapter.js
+++ backend/src/api/chapter/routes/chapter.js
@@ -1,9 +1,18 @@
-'use strict';
+"use strict";
 
 /**
  * chapter router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::chapter.chapter');
+module.exports = createCoreRouter("api::chapter.chapter", {
+  config: {
+    find: {
+      middlewares: ["api::chapter.chapter-populate"],
+    },
+    findOne: {
+      middlewares: ["api::chapter.chapter-populate"],
+    },
+  },
+});
