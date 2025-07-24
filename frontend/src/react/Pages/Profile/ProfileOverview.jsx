7e49f4460e231cf5b6970d2a41b82cbcb92660a6
Joh <132756126+JohPy@users.noreply.github.com>
2025-07-24 11:42:48 +0200

    Refactor ProfileOverview component to disable settings button

--- frontend/src/react/Pages/Profile/ProfileOverview.jsx
+++ frontend/src/react/Pages/Profile/ProfileOverview.jsx
@@ -1,30 +1,26 @@
 import React from 'react'
 
-import { useNavigate } from 'react-router-dom'
-
 import { Stack, Button } from '@mui/material'
 import { AppSettingsAlt as SettingsIcon } from '@mui/icons-material'
 
-const ProfileOverview = () => {
-  const navigate = useNavigate()
+const buttonDisabled = true
 
-  return (
-    <Stack
-      sx={{
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center'
-      }}
+const ProfileOverview = () => (
+  <Stack
+    sx={{
+      width: '100%',
+      height: '100%',
+      justifyContent: 'center'
+    }}
+  >
+    <Button
+      variant="contained"
+      startIcon={<SettingsIcon />}
+      disabled={buttonDisabled}
     >
-      <Button
-        variant="contained"
-        startIcon={<SettingsIcon />}
-        onClick={() => navigate('/profile/settings')}
-      >
-        Go to Settings
-      </Button>
-    </Stack>
-  )
-}
+      Go to Settings
+    </Button>
+  </Stack>
+)
 
 export default ProfileOverview
